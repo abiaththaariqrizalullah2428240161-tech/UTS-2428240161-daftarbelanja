e35d8541c48f90f2e0e0b5ca0987f2b5d0931b1f
Obertsean <Obertsean_2327240040@mhs.mdp.ac.id>
2026-04-14 11:48:51 +0700

    comitpertama

---
+++ script.js
@@ -0,0 +1,56 @@
+let nama = document.getElementById("nama");
+let barang = document.getElementById("barang");
+let jumlah = document.getElementById("jumlah");
+
+
+function simpan() {
+    //jika local sotrage belum ada isi
+    if (localStorage.getItem("daftar") === null) {
+        //simpan array kosong[]
+        localStorage.setItem("daftar", "[]")
+    }
+
+    //panggil local sotrage (konversi string => object)
+    let data = JSON.parse(localStorage.getItem("daftar"))
+    console.log(data)
+
+    //simpan value npm dan nama ke dalam object data
+    data.push({
+        nama: nama.value,
+        barang: barang.value,
+        jumlah: jumlah.value
+    })
+    console.log(data)
+
+    //simpan data terbaru ke dalam local storage 
+    // konversi dari object menjadi string
+    localStorage.setItem("daftar", JSON.stringify(data))
+
+    //panggil tampil()
+    tampil()
+}
+
+function tampil(){
+     //panggil dulu local storage nya
+    let hasil = JSON.parse(localStorage.getItem("daftar"))
+    // clear element ul id = list-mhs
+    document.getElementById("daftar-belanja").innerHTML = ""
+    //lakukan perulangan (foreach)
+    hasil.forEach(element => {
+        document.getElementById("daftar-beloanja").innerHTML += `
+        <div class="col-lg-4 col-md-6 mb-3">
+            <div class="card p-2">
+                <img src="${element.image}" class="card-img-top" object-fit:contain style="height:200px;">
+                <div class="card-body">
+                    <h4 class="text-primary">${element.nama}</h4>
+                    <h6 class="text-primary">${element.barang}</h6>
+                    <h6 class="text-primary">${element.jumlah}</h6>
+
+                </div>
+            </div>
+        </div>`
+    });
+}
+
+//jalankan function tampil
+tampil()
